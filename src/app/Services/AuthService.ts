4837e0b1eb5bb6d01377e5d90eb1e0f9a22ea656
Nadatayel001 <nadatayel52@gmail.com>
2025-10-25 10:48:39 +0300

    handle role permissions with logout

--- src/app/Services/AuthService.ts
+++ src/app/Services/AuthService.ts
@@ -19,6 +19,7 @@ export class AuthService {
         if (response?.token) {
           localStorage.setItem('token', response.token);
           localStorage.setItem('isLoggedIn', 'true');
+           localStorage.setItem('role', response.roleName); 
         }
       })
     );
@@ -31,11 +32,25 @@ return this.http.post<any>(`${this.baseUrl}/users`, model).pipe(    tap((respons
     })
   );
 }
+isAdmin(): boolean {
+  debugger
+  return localStorage.getItem('role')?.toLowerCase() === 'admin';
+}
+
+isUser(): boolean {
+  return localStorage.getItem('role')?.toLowerCase() === 'user';
+}
+
+getCurrentRole(): string | null {
+  debugger
+  return localStorage.getItem('role');
+}
 
 
   logout(): void {
     localStorage.removeItem('token');
     localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('role');
   }
 
   isAuthenticated(): boolean {
